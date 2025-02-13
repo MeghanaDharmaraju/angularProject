6b4c5f728a089d58bc2dadfffe1157af3ca88be9
Meghana Dharmaraju <meghanamegha380@gamil.com>
2025-02-13 08:56:59 +0530

    install bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DataBindingComponent } from './data-binding/data-binding.component';
+import { DirectiveComponent } from './directive/directive.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    DataBindingComponent, 
+    // DataBindingComponent, 
+    DirectiveComponent
    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
