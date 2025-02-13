45a7141d58656a5f57eb39cb16f6e6c4c7a207fb
Meghana Dharmaraju <meghanamegha380@gamil.com>
2025-02-13 09:24:41 +0530

    *ngIf structural directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 import { DirectiveComponent } from './directive/directive.component';
+import { StructuralDirectiveNgifVsIfComponent } from './structural-directive-ngif-vs-if/structural-directive-ngif-vs-if.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     // DataBindingComponent, 
-    DirectiveComponent
+    // DirectiveComponent,
+    StructuralDirectiveNgifVsIfComponent
    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
