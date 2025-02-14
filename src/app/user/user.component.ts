25fc87d472625ed0d1c27b598831e9556b00b37e
Meghana Dharmaraju <meghanamegha380@gamil.com>
2025-02-14 09:23:07 +0530

    Routing by using navigate methods

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  
+  constructor(private router: Router) {}
 
+  loginData() {
+
+    // // user verification logic  and after that we will use router naviagte method
+    // // by using navigateUrl method
+
+    // this.router.navigateByUrl('structural-directive');
+
+    // // by using navigate method
+
+    this.router.navigate(['structural-directive']);
+  }
 }
