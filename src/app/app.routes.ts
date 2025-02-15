ca22972ba42975efbabcc17b181b24e7fafe9d46
Meghana Dharmaraju <meghanamegha380@gamil.com>
2025-02-15 08:21:32 +0530

    Built-in-pipes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,18 +4,23 @@ import { DataBindingComponent } from './data-binding/data-binding.component';
 import { StructuralDirectiveNgifVsIfComponent } from './structural-directive-ngif-vs-if/structural-directive-ngif-vs-if.component';
 import { AttributeDirectivesComponent } from './attribute-directives/attribute-directives.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { BuiltInPipesComponent } from './built-in-pipes/built-in-pipes.component';
 
 export const routes: Routes = [
   // // defalut routes
   // {path: '', component:UserComponent},
-  {path: '', redirectTo: 'user', pathMatch: 'full'},
+  { path: '', redirectTo: 'user', pathMatch: 'full' },
 
   // // normal routes
-  {path: 'user', component: UserComponent},
-  {path: 'data-binding', component: DataBindingComponent},
-  {path: 'structural-directive', component: StructuralDirectiveNgifVsIfComponent},
-  {path: 'attribute-directive', component: AttributeDirectivesComponent},
+  { path: 'user', component: UserComponent },
+  { path: 'data-binding', component: DataBindingComponent },
+  {
+    path: 'structural-directive',
+    component: StructuralDirectiveNgifVsIfComponent,
+  },
+  { path: 'attribute-directive', component: AttributeDirectivesComponent },
+  { path: 'built-in-pipes', component: BuiltInPipesComponent },
 
   // // wild card route
-  {path: '**', component:PageNotFoundComponent}
+  { path: '**', component: PageNotFoundComponent },
 ];
